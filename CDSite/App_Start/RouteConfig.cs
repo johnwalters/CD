2d76ac3d8d3696502f0065f028043b1e9c36f7ee
Erik Ring-Walters <erik.ring.walters@gmail.com>
2017-06-30 14:08:19 -0700

    Added typescript, and changed route config for APIs

--- CDSite/App_Start/RouteConfig.cs
+++ CDSite/App_Start/RouteConfig.cs
@@ -14,6 +14,20 @@ namespace CDSite
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+               name: "ApiConfirmEmail",
+               url: "Api/confirmemail",
+               defaults: new { controller = "Deal", action = "ConfirmEmailJson" }
+           );
+
+            routes.MapRoute(
+               name: "ApiRegister",
+               url: "Api/Register",
+               defaults: new { controller = "Deal", action = "RegisterJson" }
+           );
+
+            
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
